23a1c486502003e66a132a153d62294db1e6c970
Ващенко Артем Ігорович <ipz231_vai@student.ztu.edu.ua>
2024-04-21 18:29:32 +0300

    Task commit

---
+++ HelloName/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloName.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
